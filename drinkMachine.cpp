aa8e6087c7bfc56e0dd5f54730ec28631312858d
Jazmin <163955648+jazminevendorra@users.noreply.github.com>
2024-07-05 11:21:04 -0400

    Update drinkMachine.cpp

--- drinkMachine.cpp
+++ drinkMachine.cpp
@@ -1,10 +1,6 @@
 /* This program will simulate a soft drink dispenser
 It will display the name of the drinks as well as the number
 of drinks in the machine.
-
-Jazmin Even Dorra
-Intermediate C++
-
 */
 
 #include <iostream>
